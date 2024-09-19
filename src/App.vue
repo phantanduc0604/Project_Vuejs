9cd0208044f6d91ca068c34520f2dcfbe811df47
Danny <danny@vinova.com.sg>
2024-09-19 19:20:01 +0700

    First commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  data() {
+    return {
+      name: "Danny",
+      status: true,
+      tasks: [
+        'Task One', 'Task Two', 'Task Three'
+      ],
+      link: 'https://google.com '
+    }
+  },
+  methods: {
+    toggleStatus() {
+      
+    }
+  }
+}
+</script>
+
+<template>
+   <h1>{{ name }}</h1>
+   <p v-if="status">Active</p>
+   <h3>Tasks:</h3>
+   <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+   </ul>
+   <a :href="link">Click to google</a>
+   <button @click="toggleStatus">Change status</button>
+</template>
+
+
